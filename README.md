8fd06ce81c4b136b414afd41c5e19555ab08b05d
Evan Diaz <evand1998@gmail.com>
2022-03-06 23:33:02 -0600

    gitnit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Tech-Blog
+
+AS A developer who writes about tech
+I WANT a CMS-style blog site
+SO THAT I can publish articles, blog posts, and my thoughts and opinions
+
+
+![image]
+
+## Table of Contents
+
+- [Installation](#Installation)
+- [Links](#Links)
+- [Tools](#Tools)
+- [Contact Me](#Contact-Me)
+
+## Installation
+
+No installation is necessary - see the deployed Heroku Link
+
+## Links
+
+- [GitHub](https://github.com/evand1998/Tech-Blog)
+
+
+## Contact Me
+
+- [GitHub Profile](https://github.com/evand1998)
+- Email: evand1998@gmail.com
---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,85 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+// GET all Posts
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: ['id', 'title', 'post_text', 'created_at'],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+    .then(dbPostData => {
+        // Serialize the Data before passing the Template
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('dashboard', { posts, loggedIn: true });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// GET a Single Post
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'title', 'post_text', 'created_at'],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No Post found with this ID' });
+            return;
+        }
+        // Serialize the Data
+        const post = dbPostData.get({ plain: true });
+        // Pass to the template
+        res.render('edit-post', {
+            post,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/new', (req, res) => {
+    res.render('new-post');
+});
+
+module.exports = router;
